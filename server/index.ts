9352f020b264a2dc7aac33f7bbf781d6a47f3ac9
DmitroYakimov <dimayakimov18052002@gmail.com>
2024-08-02 22:01:29 +0300

    Socket update(not done)

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,49 @@
 const { Snaptrade } = require("snaptrade-typescript-sdk");
-const uuid = require("uuid");
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const socketIo = require('socket.io');
+const http = require('http');
 
 const app = express();
 
-const jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.json());
+const server = http.createServer(app);
+const io = socketIo(server);
  
 // create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+io.on('connection', (socket) => {
+  console.log('Клієнт підключився:', socket.id);
+
+  // Обробка події відключення
+  socket.on('disconnect', () => {
+    console.log('Клієнт відключився:', socket.id);
+  });
+});
 
 const snaptrade = new Snaptrade({
   consumerKey: 'lvjONDuH7231UgFgNNtdNUqMbKq7J26ToForrSZXmWJjNcy578',
   clientId: 'MVMNT-TEST',
 });
 
-app.get('/list', async (req, res) => {
+app.get('/', async (req, res) => {
+  res.send('Nothing here');
+})
+
+app.get('/check', async (req, res) => {
+  const checkResponse = await snaptrade.apiStatus.check();
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify(checkResponse.data));
+  res.end();
+})
+
+app.post('/list', async (req, res) => {
   const listSnapTradeUsersResponse = await snaptrade.authentication.listSnapTradeUsers();
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(listSnapTradeUsersResponse.data));
@@ -37,9 +65,9 @@ app.post('/listUser', urlencodedParser, async (req, res) => {
 app.post('/login', urlencodedParser, async (req, res) => {
   const loginSnapTradeUserResponse =
   await snaptrade.authentication.loginSnapTradeUser({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
-    broker: req.data.broker,
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
+    broker: req.body.broker,
     immediateRedirect: true,
     customRedirect: '',
     reconnect: "",
@@ -53,7 +81,7 @@ app.post('/login', urlencodedParser, async (req, res) => {
 
 app.post('/delete', urlencodedParser, async (req, res) => {
   const deleteSnapTradeUserResponse = await snaptrade.authentication.deleteSnapTradeUser({
-    userId: req.data.userId,
+    userId: req.body.userId,
   });
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(deleteSnapTradeUserResponse.data));
@@ -63,7 +91,7 @@ app.post('/delete', urlencodedParser, async (req, res) => {
 app.post('/reg', urlencodedParser, async (req, res) => {
   const registerSnapTradeUserResponse =
   (await snaptrade.authentication.registerSnapTradeUser({
-    userId: req.data.userId,
+    userId: req.body.userId,
   })).data;
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(registerSnapTradeUserResponse));
@@ -73,8 +101,8 @@ app.post('/reg', urlencodedParser, async (req, res) => {
 app.post('/reset', urlencodedParser, async (req, res) => {
   const resetSnapTradeUserSecretResponse =
   await snaptrade.authentication.resetSnapTradeUserSecret({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
   });
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(resetSnapTradeUserSecretResponse.data));
@@ -82,22 +110,32 @@ app.post('/reset', urlencodedParser, async (req, res) => {
 })
 
 app.post('/getAll', urlencodedParser, async (req, res) => {
-  const getAllUserHoldingsResponse =
-  await snaptrade.accountInformation.getAllUserHoldings({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
-    brokerageAuthorizations: req.data.brokerageAuthorizations,
-  });
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify(getAllUserHoldingsResponse.data));
-  res.end();
+      const getAllUserHoldingsResponse = await snaptrade.accountInformation.getAllUserHoldings({
+        userId: req.body.userId,
+        userSecret: req.body.userSecret,
+        brokerageAuthorizations: req.body.brokerageAuthorizations,
+      });
+  
+      // Відправка даних назад клієнту (або всім клієнтам)
+      io.emit('data', JSON.stringify(getAllUserHoldingsResponse.data));
+      res.status(200).send({ message: 'Дані отримано і оброблено.' });
+    // Відключення клієнта
+  // const getAllUserHoldingsResponse =
+  // await snaptrade.accountInformation.getAllUserHoldings({
+  //   userId: req.body.userId,
+  //   userSecret: req.body.userSecret,
+  //   brokerageAuthorizations: req.body.brokerageAuthorizations,
+  // });
+   //     res.writeHead(200, { 'Content-Type': 'application/json' });
+  // res.write(JSON.stringify(getAllUserHoldingsResponse.data));
+  // res.end();
 })
 
 app.post('/listBroker', urlencodedParser, async (req, res) => {
   const listBrokerageAuthorizationsResponse =
   await snaptrade.connections.listBrokerageAuthorizations({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
   });
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(listBrokerageAuthorizationsResponse.data));
@@ -107,9 +145,9 @@ app.post('/listBroker', urlencodedParser, async (req, res) => {
 app.post('/accBalance', urlencodedParser, async (req, res) => {
   const getUserAccountBalanceResponse =
   await snaptrade.accountInformation.getUserAccountBalance({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
-    accountId: req.data.accountId,
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
+    accountId: req.body.accountId,
   });
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(getUserAccountBalanceResponse.data));
@@ -119,9 +157,9 @@ app.post('/accBalance', urlencodedParser, async (req, res) => {
 app.post('/accDetails', urlencodedParser, async (req, res) => {
   const getUserAccountDetailsResponse =
   await snaptrade.accountInformation.getUserAccountDetails({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
-    accountId: req.data.accountId,
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
+    accountId: req.body.accountId,
   });
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(getUserAccountDetailsResponse.data));
@@ -131,11 +169,11 @@ app.post('/accDetails', urlencodedParser, async (req, res) => {
 app.post('/accOrders', urlencodedParser, async (req, res) => {
   const getUserAccountOrdersResponse =
   await snaptrade.accountInformation.getUserAccountOrders({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
-    state: req.data.state,
-    days: req.data.days,
-    accountId: req.data.accountId,
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
+    state: req.body.state,
+    days: req.body.days,
+    accountId: req.body.accountId,
   });
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(getUserAccountOrdersResponse.data));
@@ -143,23 +181,46 @@ app.post('/accOrders', urlencodedParser, async (req, res) => {
 })
 
 app.post('/accPositions', urlencodedParser, async (req, res) => {
-  const getUserAccountPositionsResponse =
-  await snaptrade.accountInformation.getUserAccountPositions({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
-    accountId: req.data.accountId,
+  io.on('connection', async (socket) => {
+    console.log('Клієнт підключився:', socket.id);
+    const getUserAccountPositionsResponse =
+    await snaptrade.accountInformation.getUserAccountPositions({
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
+    accountId: req.body.accountId,
   });
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify(getUserAccountPositionsResponse.data));
+  
+    // Обробка події від клієнта
+    socket.on('sendUserData', (userData) => {
+      console.log('Отримано дані від клієнта:', userData);
+  
+      // Відправка даних назад клієнту (або всім клієнтам)
+      // const updatedData = `Оновлені дані для користувача ${userData.userId} на ${new Date().toLocaleTimeString()}`;
+      socket.emit('data', JSON.stringify(getUserAccountPositionsResponse.data));
+    });
+  
+    // Відключення клієнта
+    socket.on('disconnect', () => {
+      console.log('Клієнт відключився:', socket.id);
+    });
+  });
+  // const getUserAccountPositionsResponse =
+  // await snaptrade.accountInformation.getUserAccountPositions({
+  //   userId: req.body.userId,
+  //   userSecret: req.body.userSecret,
+  //   accountId: req.body.accountId,
+  // });
+  // res.writeHead(200, { 'Content-Type': 'application/json' });
+  // res.write(JSON.stringify(getUserAccountPositionsResponse.data));
   res.end();
 })
 
 app.post('/userHoldings', urlencodedParser, async (req, res) => {
   const getUserHoldingsResponse =
   await snaptrade.accountInformation.getUserHoldings({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
-    accountId: req.data.accountId,
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
+    accountId: req.body.accountId,
   });
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(getUserHoldingsResponse.data));
@@ -168,9 +229,9 @@ app.post('/userHoldings', urlencodedParser, async (req, res) => {
 
 app.post('/orderImpact', urlencodedParser, async (req, res) => {
   const getOrderImpactResponse = await snaptrade.trading.getOrderImpact({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
-    accountId: req.data.accountId,
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
+    accountId: req.body.accountId,
     action: "BUY",
     order_type: "Limit",
     price: 31.33,
@@ -187,10 +248,10 @@ app.post('/orderImpact', urlencodedParser, async (req, res) => {
 app.post('/accQuotes', urlencodedParser, async (req, res) => {
   const getUserAccountQuotesResponse =
   await snaptrade.trading.getUserAccountQuotes({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
-    symbols: req.data.symbols,
-    accountId: req.data.accountId,
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
+    symbols: req.body.symbols,
+    accountId: req.body.accountId,
   });
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(getUserAccountQuotesResponse.data));
@@ -199,9 +260,9 @@ app.post('/accQuotes', urlencodedParser, async (req, res) => {
 
 app.post('/listHoldings', urlencodedParser, async (req, res) => {
   const listOptionHoldingsResponse = await snaptrade.options.listOptionHoldings({
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
-    accountId: req.data.accountId,
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
+    accountId: req.body.accountId,
   });
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(listOptionHoldingsResponse.data));
@@ -211,19 +272,21 @@ app.post('/listHoldings', urlencodedParser, async (req, res) => {
 app.post('/getActivities', urlencodedParser, async (req, res) => {
   const getActivitiesResponse =
   await snaptrade.transactionsAndReporting.getActivities({
-    startDate: req.data.startDate,
-    endDate: req.data.endDate,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
     accounts:
-    req.data.accountId,
+    req.body.accountId,
     brokerageAuthorizations:
-    req.data.brokerageAuthorizations,
-    type: req.data.type,
-    userId: req.data.userId,
-    userSecret: req.data.userSecret,
+    req.body.brokerageAuthorizations,
+    type: req.body.type,
+    userId: req.body.userId,
+    userSecret: req.body.userSecret,
   });
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(getActivitiesResponse.data));
   res.end();
 })
 
-app.listen(4000);
+server.listen(process.env.PORT || 5000);
+
+console.log("Server is working");
